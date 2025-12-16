10e4dada6095bad587e21adae07c4e61f2e634b4
katikalasaikiran <katikalasaikiran@gmail.com>
2025-12-17 03:49:44 +0530

    Fix file import case sensitivity

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Book = require("../models/Book");
+const Book = require("../models/book");
 
 // 1. GET ALL BOOKS
 router.get("/", async (req, res) => {
